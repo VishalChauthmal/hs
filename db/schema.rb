12fc79e02eb5f00020efa3f5485fb1e6b72b998c
sushantkumar23 <sushantkumar23@gmail.com>
2015-12-10 14:33:34 +0530

    Email duplication error resolved

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151208074113) do
+ActiveRecord::Schema.define(version: 20151209193519) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,5 +22,7 @@ ActiveRecord::Schema.define(version: 20151208074113) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
