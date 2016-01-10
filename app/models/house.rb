a7d1ba24caeab014e488d9a68478d680393abcf2
Vishal25 <vishalchauthmal.com>
2016-01-10 22:05:49 +0530

    Create Houses controller and implement House Search algorithm

--- app/models/house.rb
+++ app/models/house.rb
@@ -8,4 +8,19 @@ class House < ActiveRecord::Base
 	validates :allowed_gender, presence: true
 	validates :locality_id, presence: true
 	validates :address, presence: true
+
+	def self.search_results(search_params)
+		locality_id = search_params[:locality_id]
+		gender = search_params[:gender]
+
+		if !locality_id.blank? && !gender.blank?		# Locality & Gender both specified
+			House.where("locality_id IN (#{locality_id}) AND allowed_gender IN ('#{gender}')")
+		elsif locality_id.blank? && !gender.blank?	# Only Gender specified
+			House.where("allowed_gender IN ('#{gender}')")
+		elsif !locality_id.blank? && gender.blank?	# Only Locality specified
+			House.where("locality_id IN (#{locality_id})")
+		else																				# Neither Locality nor Gender specified
+			House.all
+		end
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 Rails.application.routes.draw do
 
-    get 'signup' => 'users#new'
+  get 'search' => 'houses#search'
+  get 'results' => 'houses#results'
 
-    root 'home#index'
+  root 'home#index'
 
-    get 'home/index'
-    get 'home/search'
-    
-    resources :users
+  get 'home/index'
+  get 'home/search'
+
+
+  get 'signup' => 'users#new'
+	resources :users
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
