bc960d610473342df80dacbf7260d10e4949e7e3
Vishal25 <vishalchauthmal.com>
2016-01-19 15:51:42 +0530

    Email not required in form

--- app/models/house_listing_request.rb
+++ app/models/house_listing_request.rb
@@ -3,6 +3,11 @@ class HouseListingRequest < ActiveRecord::Base
 
 	validates :name, presence: true, length: { maximum: 100 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-	validates :email, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
+	# validates :email, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
 	validates :phone, presence: true
+
+	protected
+	def email_required?
+		false
+	end
 end
