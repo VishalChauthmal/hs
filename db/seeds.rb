529a205b9281bcab6a9ff4e359edf6901c9d7413
Vishal25 <vishalchauthmal.com>
2016-02-11 18:09:49 +0530

    Change Raj Splendour data

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,7 +26,7 @@ House.create!(#type, owner,
 							#pincode, lat, long, landmark,
 							address: "Raj Legacy Housing Cooperative Society, LBS Road, Vikhroli West - 400079")
 House.create!(title: "Raj Splendour", bhk: 2, max_no_of_beds: 4, locality_id: 1, rent: 49000, security_deposit: 130000, 
-							address: "Raj splendour, LBS Road, Vikhroli West - 400079")
+							address: "Raj Splendour, LBS Road, Vikhroli West - 400079")
 House.create!(title: "Lodha Aurum", bhk: 2, max_no_of_beds: 4, locality_id: 2, rent: 45000, security_deposit: 200000,
 							address: "Lodha Aurum Grande, Kanjurmarg East - 400042")
 
@@ -48,7 +48,8 @@ House.find(3).house_tenant_type_relationships.create!(tenant_type_id: 1)
 
 2.times { Room.create!(house_id: 1, #tenant_id,
 											rent: 47000/2, security_deposit: 200000/2, max_room_occupancy: 2) }
-2.times { Room.create!(house_id: 2, rent: 49000/2, security_deposit: 130000/2, max_room_occupancy: 2) }
+Room.create!(house_id: 2, rent: 24000, security_deposit: 130000/2, max_room_occupancy: 2)
+Room.create!(house_id: 2, rent: 26000, security_deposit: 130000/2, max_room_occupancy: 2)
 2.times { Room.create!(house_id: 3, rent: 45000/2, security_deposit: 200000/2, max_room_occupancy: 2) }
 
 
@@ -56,8 +57,8 @@ House.find(3).house_tenant_type_relationships.create!(tenant_type_id: 1)
 											rent: 47000/4, security_deposit: 200000/4) }
 2.times { Bed.create!(room_id: 2, #tenant_id,
 											rent: 47000/4, security_deposit: 200000/4) }
-2.times { Bed.create!(room_id: 3, rent: 49000/4, security_deposit: 130000/4) }
-2.times { Bed.create!(room_id: 4, rent: 49000/4, security_deposit: 130000/4) }
+2.times { Bed.create!(room_id: 3, rent: 12000, security_deposit: 130000/4) }
+2.times { Bed.create!(room_id: 4, rent: 13000, security_deposit: 130000/4) }
 2.times { Bed.create!(room_id: 5, rent: 45000/4, security_deposit: 200000/4) }
 2.times { Bed.create!(room_id: 6, rent: 45000/4, security_deposit: 200000/4) }
 
